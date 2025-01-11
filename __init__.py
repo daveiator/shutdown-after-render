12c5e969d880c05d6cd0a6583a9b779e86da6cc9
David Bühler <david@buehler-online.de>
2025-01-11 16:11:24 +0100

    moved extras section to render topbar. Cleaned up code

--- __init__.py
+++ __init__.py
@@ -29,16 +29,16 @@ bpy.types.WindowManager.shutdown_modes = bpy.props.EnumProperty(
     default = 0,
     description = "Choose Shutdown Mode",
     items = [
-        ("shutdown", "Shutdown", "Shuts down the computer"),
-        ("hibernate", "Hibernate", "Sets the computer to hibernate"),
-        ("quit", "Quit Blender", "Closes Blender"),
+        ('shutdown', "Shutdown", "Shuts down the computer"),
+        ('hibernate', "Hibernate", "Sets the computer to hibernate"),
+        ('quit', "Quit Blender", "Closes Blender"),
     ]
 )
 
 #  Preferences
 
 class AddonPreferences(bpy.types.AddonPreferences):
-    bl_idname = __name__
+    bl_idname = __package__
 
     # Shutdown command
     shutdown_command: bpy.props.StringProperty(
@@ -60,31 +60,35 @@ class AddonPreferences(bpy.types.AddonPreferences):
 
     def draw(self, context):
         layout = self.layout
-        layout.label(text="Shutdown Command:")
-        layout.prop(self, "shutdown_command", text="")
-        layout.label(text="Hibernate Command:")
-        layout.prop(self, "hibernate_command", text="")
-        layout.label(text="Abort Command:")
-        layout.prop(self, "abort_command", text="")
+        layout.label(text = "Shutdown Command:")
+        layout.prop(self, 'shutdown_command', text = "")
+        layout.label(text = "Hibernate Command:")
+        layout.prop(self, 'hibernate_command', text = "")
+        layout.label(text = "Abort Command:")
+        layout.prop(self, 'abort_command', text = "")
 
 # Functions
 
 def shutdown(mode):
-    if mode == "shutdown":
+    if mode == 'shutdown':
         shutdown_command = bpy.context.preferences.addons[__package__].preferences.shutdown_command
         subprocess.run(shutdown_command)
         bpy.types.WindowManager.shutdown_in_process = True
-    elif mode == "hibernate":
+    elif mode == 'hibernate':
         hibernate_command = bpy.context.preferences.addons[__package__].preferences.hibernate_command
         subprocess.run(hibernate_command)
         bpy.types.WindowManager.shutdown_in_process = True
-    elif mode == "quit":
+    elif mode == 'quit':
         bpy.ops.wm.quit_blender()
+
+def draw_render_still_to_disk(self, context):
+    layout = self.layout
+    layout.operator('wd.render_still_to_disk', text = "Render Image to Disk", icon = 'RENDER_STILL')
 
 # Operators
 class AbortShutdown(bpy.types.Operator):
     bl_idname = 'wd.abort_shutdown'
-    bl_label = 'Abort Shutdown'
+    bl_label = "Abort Shutdown"
  
     @classmethod
     def description(cls, context, properties):
@@ -97,34 +101,22 @@ class AbortShutdown(bpy.types.Operator):
         print("Shutdown aborted!")
         return {'FINISHED'}
 
-class RenderStillToOutput(bpy.types.Operator):
-    bl_idname = 'wd.render_still'
-    bl_label = 'Render Image to Output Folder'
+class RenderStillToDisk(bpy.types.Operator):
+    bl_idname = 'wd.render_still_to_disk'
+    bl_label = "Render Image to Disk"
 
     @classmethod
     def description(cls, context, properties):
-        return "Saves the rendered image automatically to the output folder"
+        return "Renders active scene to output path"
 
     def execute(self, context):
         bpy.ops.render.render('INVOKE_DEFAULT', animation=False, write_still=True)
         return {'FINISHED'}
 
-class RenderAnimation(bpy.types.Operator):
-    bl_idname = 'wd.render_animation'
-    bl_label = 'Render Animation to Output Folder'
-
-    @classmethod
-    def description(cls, context, properties):
-        return "Renders the animation automatically to the output folder"
-
-    def execute(self, context):
-        bpy.ops.render.render('INVOKE_DEFAULT',animation=True)
-        return {'FINISHED'}
-
 # Handlers
 
 @persistent
-def render_complete(scene):
+def render_write(scene):
     if bpy.context.window_manager.arm_shutdown:
         print("Shutdown after render enabled!\nShutting down...")
         shutdown(bpy.context.window_manager.shutdown_modes)
@@ -151,7 +143,7 @@ class RENDER_PT_ShutdownPanel(ShutdownPanelContainer, bpy.types.Panel):
 
     def draw_header(self,context):
         layout = self.layout
-        layout.prop(context.window_manager, 'arm_shutdown', text='', icon='QUIT')
+        layout.prop(context.window_manager, 'arm_shutdown', text = '', icon = 'QUIT')
 
 
     def draw(self, context):
@@ -177,34 +169,21 @@ class RENDER_PT_ShutdownPanel(ShutdownPanelContainer, bpy.types.Panel):
             row = layout.row()
         pass
 
-# Extras Panel
-class RENDER_PT_ShutdownExtrasPanel(ShutdownPanelContainer, bpy.types.Panel):
-    bl_parent_id = 'RENDER_PT_ShutdownPanel'
-    bl_label = 'Extras'
-
-    def draw(self, context):
-        layout = self.layout
-        row = layout.row()
-        row.operator('wd.render_still', text="Render Image to Output Folder", icon='RENDER_STILL')
-        row = layout.row()
-        row.operator('wd.render_animation', text="Render Animation to Output Folder", icon='RENDER_ANIMATION')
-
-
-
 # Register
 
 classes = (
     AddonPreferences,
     RENDER_PT_ShutdownPanel,
-    RENDER_PT_ShutdownExtrasPanel,
     AbortShutdown,
-    RenderStillToOutput,
-    RenderAnimation)
+    RenderStillToDisk
+    )
 
 def register():
     # Handlers
     bpy.app.handlers.render_init.append(render_init)
-    bpy.app.handlers.render_complete.append(render_complete)
+    bpy.app.handlers.render_complete.append(render_write)
+    
+    bpy.types.TOPBAR_MT_render.prepend(draw_render_still_to_disk)
 
     for cls in classes:
         bpy.utils.register_class(cls)
@@ -212,10 +191,12 @@ def register():
 def unregister():
     # Handlers
     bpy.app.handlers.render_init.remove(render_init)
-    bpy.app.handlers.render_complete.remove(render_complete)
+    bpy.app.handlers.render_complete.remove(render_write)
+
+    bpy.types.TOPBAR_MT_render.remove(draw_render_still_to_disk)
 
     for cls in classes:
         bpy.utils.unregister_class(cls)
 
-if __package__ == "__main__":
+if __package__ == '__main__':
     register()
